a0a852e1aa28f885a0104eaabd007dbe342ca5f1
[Thirumalesh Pinninti] <[thirumaleshpinninti@gmail.com]>
2025-05-23 08:44:21 +0530

    updated

--- api/qrcodes.js
+++ api/qrcodes.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL; // Set in Vercel dashboard or .env.local
-const supabaseKey = process.env.SUPABASE_ANON_KEY; // Set in Vercel dashboard or .env.local
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default async function handler(req, res) {
